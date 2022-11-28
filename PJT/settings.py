e3501119ee062d2e9f4b62f836255d7dc5d32faf
LEE DONG YEONG <kinzinzz@naver.com>
2022-11-28 10:28:44 +0900

    test

--- PJT/settings.py
+++ PJT/settings.py
@@ -116,7 +116,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -129,8 +128,6 @@ STATICFILES_DIRS = [
 # Media files (user uploaded files)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'images'
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
