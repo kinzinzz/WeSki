816607273cba22bf42c975a16a01993113034d44
LEE DONG YEONG <kinzinzz@naver.com>
2022-11-28 10:26:02 +0900

    build:static 폴더 추가2

--- PJT/urls.py
+++ PJT/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('reviews/', include('reviews.urls')),
     path('', include('places.urls')),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
