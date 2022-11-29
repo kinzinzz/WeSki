efdb71bf9a9c8dc91fd4c3327d0f57ae40c2292e
JungAh Choi <jungahchoi92@gmail.com>
2022-11-29 09:24:55 +0900

    fix: UserChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 
@@ -6,3 +6,11 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ("username",)
+
+
+class ChangeUserForm(UserChangeForm):
+        password = None
+
+        class Meta:
+            model = get_user_model()
+            fields = ("image", "email")
