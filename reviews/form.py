180d97ffb95dead0c055029106f4568c2b320a8f
LEE DONG YEONG <kinzinzz@naver.com>
2022-12-01 10:01:45 +0900

    feat:reviews form place choice update from dy

--- reviews/form.py
+++ reviews/form.py
@@ -1,8 +1,13 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Review
 from .widgets import starWidget
+from places.models import Place
 
 class Review_form(ModelForm):
+    
+    place = forms.ModelChoiceField(queryset=Place.objects.all(),label="스키장")
+    
     class Meta:
         model=Review
         exclude=("likes","user","likes_num")
