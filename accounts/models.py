3d157efdb59240e016263055eda68b2999a1997b
1c0332zz <1c0332zz@gmail.com>
2022-12-06 18:01:24 +0900

    feat : 네비바 업데이트

--- accounts/models.py
+++ accounts/models.py
@@ -15,15 +15,23 @@ class User(AbstractUser):
         options={"quality": 90},
     )
 
+    def profile_image(self):
+        if self.image and hasattr(self.image, "url"):
+            return self.image.url
+        else:
+            return "https://i.esdrop.com/d/f/bvRLlOwptP/D1wDSBrqJO.jpg"
+
 
 # 결제 정보
 class OrderTransaction(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    place = models.ForeignKey(Place, on_delete=models.PROTECT, related_name='order_places')
+    place = models.ForeignKey(
+        Place, on_delete=models.PROTECT, related_name="order_places"
+    )
     price = models.IntegerField()
     order_date = models.DateTimeField(auto_now_add=True)
     imp_uid = models.CharField(max_length=50)
     merchant_uid = models.CharField(max_length=50)
 
     def __str__(self):
-        return '{}'.format(self.id)
+        return "{}".format(self.id)
