a9fe2495593fbc7ff567a7ffc5e6b3496db9f87d
slugjeon <youngman0515@naver.com>
2022-12-06 14:12:34 +0900

    fix: review detail페이지 복구

--- reviews/urls.py
+++ reviews/urls.py
@@ -12,12 +12,11 @@ urlpatterns = [
     # re_path(r"(?P<review_pk>\d+)\Z",views.detail,name="detail"),
     # re_path(r'create/?(?P<from>[a-zA-Z_]*)/?(?P<number>\d*)\Z',views.create,name="create"),
     # path("",views.index,name="index"),
-    # path("<int:review_pk>/",views.detail,name="detail"),
-    # path("<int:review_pk>/update/",views.update,name="update"),
+    path("<int:review_pk>/",views.detail,name="detail"),
+    path("<int:review_pk>/update/",views.update,name="update"),
     path('create/',views.create,name="create"),
     path("<int:review_pk>/delete/",views.delete, name="delete"),
     path("<int:review_pk>/likes/",views.likes,name="likes"),
     path("<int:review_pk>/<int:reviewimage_pk>/image_delete/",views.image_delete,name="image_delete"),
     path("<int:review_pk>/image_add/",views.image_add,name="image_add"),
-    
     ]
